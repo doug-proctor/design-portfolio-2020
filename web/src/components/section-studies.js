0a74334ce58ca887ee24e6e936f6c038b3be1a5b
Doug Proctor <doug-proctor@users.noreply.github.com>
2020-07-07 19:55:32 +0100

    Switched to gatsby-image for project preview images

--- web/src/components/section-studies.js
+++ web/src/components/section-studies.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import {Link} from 'gatsby'
-import {buildImageObj} from '../lib/helpers'
-import {imageUrlFor} from '../lib/image-url'
+import Img from 'gatsby-image'
+import { getFluidGatsbyImage } from 'gatsby-source-sanity'
 
+import clientConfig from '../../client-config'
 import styles from './section-studies.module.css'
 
 const SectionStudies = ({ studies }) => {
@@ -10,17 +11,18 @@ const SectionStudies = ({ studies }) => {
     <div className={styles.root}>
       <h2 className={styles.title}>UX case studies</h2>
       <div className={styles.grid}>
-        {studies.map(study => (
-          <Link key={study.id} className={styles.cardLink} to={`studies/${study.slug.current}`}>
-            <div className={styles.card}>
-              <img className={styles.cardImage} src={imageUrlFor(buildImageObj(study.previewImage))
-                .width(600)
-                .url()} alt={`${study.client} case study thumbnail`}/>
-              <h3 className={styles.cardTitle}>{study.client}</h3>
-              <h4 className={styles.cardSubtitle}>{study.title}</h4>
-            </div>
-          </Link>
-        ))}
+        {studies.map(study => {
+          const fluidProps = getFluidGatsbyImage(study.previewImage.asset._id, {maxWidth: 375}, clientConfig.sanity)
+          return (
+            <Link key={study.id} className={styles.cardLink} to={`studies/${study.slug.current}`}>
+              <div className={styles.card}>
+                <Img fluid={fluidProps} alt={`${study.client} case study preview image`} />
+                <h3 className={styles.cardTitle}>{study.client}</h3>
+                <h4 className={styles.cardSubtitle}>{study.title}</h4>
+              </div>
+            </Link>
+            )
+        })}
       </div>
     </div>
   )
